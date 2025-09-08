cf6062bea034fd989f2fb7c808f6105e24b9dcaa
Mayank <122718301+CodeVoot@users.noreply.github.com>
2025-09-08 22:43:23 +0530

    Delete Springboot by juha hinkula-20250828T101251Z-1-001/Springboot by juha hinkula directory

--- Springboot by juha hinkula-20250828T101251Z-1-001/Springboot by juha hinkula/Cardatabase/Cardatabase/src/main/java/com/example/Cardatabase/domain/CarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Cardatabase.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-                  // Service
-public interface CarRepository extends CrudRepository<Car, Long>{
-	
-}
